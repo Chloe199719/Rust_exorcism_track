b52c6875ebe234922dda125bd02a871a231b479d
Chloe Pratas <chloevision97@gmail.com>
2023-07-01 05:56:58 +0200

    Optimizing the code for the grains exercise.
    ```rust

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -3,21 +3,10 @@ pub fn square(s: u32) -> u64 {
     if s == 0 || s > 64 {
         panic!("Square must be between 1 and 64");
     }
-    let mut hashMap = HashMap::new();
-    for i in 1..65 {
-        hashMap.insert(i, 2u64.pow(i-1));
-    }
-    *hashMap.get(&s).unwrap()
+    2u64.pow(s-1)
+ 
 }
 
 pub fn total() -> u64 {
-    let mut hashMap = HashMap::new();
-    for i in 1..65 {
-        hashMap.insert(i, 2u64.pow(i-1));
-    }
-    let mut sum: u64 = 0;
-    for i in 1..65 {
-        sum += *hashMap.get(&i).unwrap();
-    }
-    sum
+    u64::max_value()
 }
